9ab49aa00457e31b94675ff11f41e0856a118d18
danielberenberg <djberenb@uvm.edu>
2019-09-10 13:01:20 -0400

    packaging probs

--- src/defunct/defunct/__init__.py
+++ src/defunct/defunct/__init__.py
@@ -1,3 +1,7 @@
 from . import de, funct, utils
 
+decorators = de
+funcs      = funct # maintain backwards compat 
+
+__all__ = ['de', 'funct', 'utils', 'decorators', 'funcs']
 
--- src/defunct/setup.py
+++ src/defunct/setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(name="defunct",
-      version="0.2",
+      version="0.23",
       author="Daniel Berenberg",
       description="decorators + functions",
       packages=find_packages())
